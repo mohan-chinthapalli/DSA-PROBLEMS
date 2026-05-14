d4720f49dd47d94d53d1b392d0000e9919d4d9c6
Monnnoha <chinthapallimohan19@gmail.com>
2026-05-14 22:33:31 +0530

    DAY 14 - LC268 optimal

--- LC268.java
+++ LC268.java
@@ -4,14 +4,23 @@ public class LC268 {
         int[] nums = {3, 0, 1};
         int n = nums.length;
 
-        int actualSum = n * (n+1) /2;
-        int prefixSum = 0;
-        for(int num : nums){
-            prefixSum += num;
+        // int actualSum = n * (n+1) /2;
+        // int prefixSum = 0;
+        // for(int num : nums){
+        //     prefixSum += num;
+        // }
+
+        // // Missing Number  solution with sum idea
+        // int missingNumber = actualSum - prefixSum;
+
+        // System.out.println(missingNumber);
+
+        int result = nums.length;
+        for(int i=0; i<nums.length; i++){
+            result ^= i;
+            result ^= nums[i];
         }
 
-        int missingNumber = actualSum - prefixSum;
-
-        System.out.println(missingNumber);
+        System.out.println(result);
     }
 }
