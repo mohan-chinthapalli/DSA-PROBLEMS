4b2c64b9db40e6221e9c3f2a7da29e4c57e409ab
Monnnoha <chinthapallimohan19@gmail.com>
2026-05-14 10:51:48 +0530

    DAY 13 - LC136

--- LC136.java
+++ LC136.java
@@ -4,9 +4,15 @@ public class LC136 {
     public static void main(String[] args) {
         // HashMap<Integer, Integer> map = new HashMap<>();
 
-        int[] nums = {2, 2, 1};
+        int[] nums = {4, 1, 2, 1, 2};
         int n = nums.length;
 
+        // Optimal Aprroach
+        int prefixXor = 0;
+        for(int i=0; i<n; i++){
+            prefixXor = prefixXor ^ nums[i];
+        }
+        System.out.println(prefixXor);
         // brute approach
         // int remains = 0;
 
