333927776da6fe20bdcd6592af8fd86e8f2461a7
Monnnoha <chinthapallimohan19@gmail.com>
2026-05-14 10:43:34 +0530

    DAY 13 - LC136

---
+++ LC136.java
@@ -0,0 +1,23 @@
+
+
+public class LC136 {
+    public static void main(String[] args) {
+        // HashMap<Integer, Integer> map = new HashMap<>();
+
+        int[] nums = {2, 2, 1};
+        int n = nums.length;
+
+        // brute approach
+        // int remains = 0;
+
+        // for(int i=0; i<n; i++){
+        //     map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+
+        //     if(map.get(nums[i]) == 1){
+        //         remains = nums[i];
+        //     }
+        // }
+
+        // System.out.println(remains);
+    }
+}
