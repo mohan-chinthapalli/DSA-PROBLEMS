9df569ee2813c0a309517457a428dd24a49fbea0
Monnnoha <chinthapallimohan19@gmail.com>
2026-05-15 15:17:47 +0530

    DAY 14 - LC1442 brute and optimal approaches

--- LC1442.java
+++ LC1442.java
@@ -6,26 +6,36 @@ public class LC1442 {
         int count=0;
        
 
-        int i=0;
-        int j=i+1;
-        for( i=0; i<n; i++){
-            for(j=i+1; j<n; j++){
-                for(int k=j; k<n; k++){
+        // int i=0;
+        // int j=i+1;
+        // for( i=0; i<n; i++){
+        //     for(j=i+1; j<n; j++){
+        //         for(int k=j; k<n; k++){
 
-                    int a = 0;
-                    int b=0;
-                    for(int x=i; x<j; x++){
-                     a= a^arr[x];
-                    }
+        //             int a = 0;
+        //             int b=0;
+        //             for(int x=i; x<j; x++){
+        //              a= a^arr[x];
+        //             }
 
-                    for(int x=j; x<=k; x++){
-                     b=b^arr[x];
-                    }
+        //             for(int x=j; x<=k; x++){
+        //              b=b^arr[x];
+        //             }
                 
-                if(a==b){
-                    count++;
+        //         if(a==b){
+        //             count++;
+        //         }
+        //     }
+        //     }
+        // }
+        // optimal approach
+        for(int i=0; i<n; i++){
+            int xor = 0;
+            for(int k=i; k<n; k++){
+                xor  ^= arr[k];
+                if(xor == 0){
+                    count+=(k-i);
                 }
-            }
             }
         }
         System.out.println(count);
