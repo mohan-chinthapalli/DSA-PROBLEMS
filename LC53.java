3d84c98f8badfc122b3d112d336f8dcb7353e0e7
Monnnoha <chinthapallimohan19@gmail.com>
2026-05-18 22:02:11 +0530

    DAY 15

--- LC53.java
+++ LC53.java
@@ -1,16 +1,25 @@
 public class LC53 {
     public static void main(String[] args) {
         int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
-        int sum = 0;
-        int max = Integer.MIN_VALUE;
+        // brute approach
+        // for(int i=0; i<nums.length; i++){
+        //     sum=0;
+        //     for(int j=i; j<nums.length; j++){
+        //         sum+=nums[j];
+        //         max = Math.max(max, sum);
+        //     }
+        // }
+        // most optimal approach
+        int currSum = 0;
+        int maxSum = Integer.MIN_VALUE;
         for(int i=0; i<nums.length; i++){
-            sum=0;
-            for(int j=i; j<nums.length; j++){
-                sum+=nums[j];
-                max = Math.max(max, sum);
+            currSum += nums[i];
+            maxSum = Math.max(currSum, maxSum);
+            if(currSum<0){
+                currSum = 0;
             }
-        }
 
-        System.out.println(max);
+        }
+        System.out.println(maxSum);
     }
 }
