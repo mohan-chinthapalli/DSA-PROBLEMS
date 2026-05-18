cce5d7151f5427ecbb0a023f9bc144080a3a14c2
Monnnoha <chinthapallimohan19@gmail.com>
2026-05-18 11:16:44 +0530

    DAY - 15

---
+++ LC238.java
@@ -0,0 +1,21 @@
+public class LC238 {
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4};
+        // brute force approach
+        // int[] res = new int[nums.length];
+        // int product = 1;
+        // for(int i=0; i<nums.length; i++){
+        //     product = 1;
+        //     for(int j=0; j<nums.length; j++){
+        //         if(j!=i){
+        //             product *= nums[j];
+        //             res[i]=product;
+        //         }
+        //     }    
+        // }
+
+        // for(int i=0; i<res.length; i++){
+        //     System.out.print(res[i] + " ");
+        // }
+    }
+}
