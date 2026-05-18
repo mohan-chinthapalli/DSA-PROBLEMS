614087cfa2c88471f3a895617e59747433709124
Monnnoha <chinthapallimohan19@gmail.com>
2026-05-18 11:56:56 +0530

    DAY 15

--- LC238.java
+++ LC238.java
@@ -17,5 +17,26 @@ public class LC238 {
         // for(int i=0; i<res.length; i++){
         //     System.out.print(res[i] + " ");
         // }
+
+        int[] prefix = new int[nums.length];
+        int[] suffix = new int[nums.length];
+
+        prefix[0] = 1;
+        for(int i=1; i<nums.length; i++){
+            prefix[i] = prefix[i-1] * nums[i-1];
+        }
+        suffix[nums.length-1] = 1;
+        for(int i=nums.length-2; i>=0; i--){
+            suffix[i] = suffix[i+1] * nums[i+1];
+        }
+
+        for(int i=0; i<nums.length; i++){
+            nums[i] = prefix[i] * suffix[i];
+        }
+
+        for(int i=0; i<nums.length; i++){
+            System.out.print(nums[i] + " ");
+        }
+
     }
 }
