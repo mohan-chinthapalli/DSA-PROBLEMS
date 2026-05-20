923da7ac8200faf7ec710f1b0afaca9628d8c726
Monnnoha <chinthapallimohan19@gmail.com>
2026-05-20 09:54:15 +0530

    DAY 17

--- LC1524.java
+++ LC1524.java
@@ -1,6 +1,6 @@
 public class LC1524 {
     public static void main(String[] args) {
-        int[] nums = {1, 2, 3, 4, 5, 6, 7};
+        int[] nums = {1, 3, 5};
 
         int count =0;
         int sum =0;
@@ -15,6 +15,23 @@ public class LC1524 {
         //         }
         //     }
         // }
+
+        int even = 1;
+        int odd = 0;
+        for(int i=0; i<nums.length; i++){
+            sum+=nums[i];
+            if(sum%2==0){
+                count+=odd;
+            }else{
+                count+=even;
+            }
+
+            if(sum%2==0){
+                even++;
+            }else{
+                odd++;
+            }
+        }
         System.out.println(count);
     }
 }
