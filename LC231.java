cf44b693c572c8975c0389f08daa5aff0281688d
Monnnoha <chinthapallimohan19@gmail.com>
2026-05-27 11:48:19 +0530

    DAY 20

--- LC231.java
+++ LC231.java
@@ -1,3 +1,22 @@
 public class LC231 {
-    
+    public static void main(String[] args) {
+        int n = 0;
+        int digit = n;
+
+        while(digit!=1){
+            if(digit%2==0){
+            digit=digit/2;
+            }else{
+                break;
+            }
+        }
+
+        if(digit==1){
+
+            System.out.println(true);
+        }else{
+            System.out.println(false);
+        }
+
+    }
 }
