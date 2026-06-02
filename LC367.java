56c14fa8d49c249d463864f41d119c4ad4bca5f9
Chinthapalli Mohan <chinthapallimohan19@gmail.com>
2026-06-02 21:09:07 +0530

    DAY - 22

--- LC367.java
+++ LC367.java
@@ -1,3 +1,22 @@
 public class LC367 {
-    
+    public static void main(String[] args) {
+        // optimal approach with binary search O(log N)
+        int num = 16;
+        int st = 1;
+        int end = num;
+        int mid=0;
+
+        while(st<=end){
+            mid=st+(end-st)/2;
+            if((mid*mid)>num){
+                end = mid-1;
+            }else if((mid*mid)<num){
+                st=mid+1;
+            }else{
+                System.out.println(true);
+                return;
+            }
+        }
+        System.out.println(false);
+    }
 }
