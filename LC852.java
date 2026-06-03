0156f7561835063e2a19b6018c3e1be2219e9100
Chinthapalli Mohan <chinthapallimohan19@gmail.com>
2026-06-03 11:31:27 +0530

    DAY - 23

--- LC852.java
+++ LC852.java
@@ -7,16 +7,13 @@ public class LC852 {
         int mid=0; 
         int ans = 0; 
 
-        while(st<=end){
+        while(st<end){
             mid=st+(end-st)/2;
             if(nums[mid]<nums[mid+1]){
                 st = mid+1;
             }else if(nums[mid]>nums[mid+1]){
                 ans = mid;
                 end=mid-1;
-            }else{
-                System.out.println(mid);
-                return;
             }
         }
         System.out.println(ans);
